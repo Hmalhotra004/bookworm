fb98588749ab41644376057f407d2f75847e0679
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-04-26 19:10:07 +0530

    timestamps

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -2,27 +2,30 @@ import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 import { IUser } from "../lib/types";
 
-const userSchema = new mongoose.Schema<IUser>({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  profileImage: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
