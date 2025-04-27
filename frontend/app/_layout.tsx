368ef78a197ebfc0d49f5129a5491fa3fe56d4c4
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-04-27 22:38:50 +0530

    rootlayout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,15 @@
+import SafeScreen from "@/components/SafeScreen";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </SafeAreaProvider>
+  );
 }
